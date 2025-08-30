55434fef729685f4754871083e39dcb0e93a4ff9
Jonas Lim <jonaslim@Jonass-MacBook-Pro.local>
2025-08-30 14:40:21 +1000

    [backend] save work on ocr pipeline

--- backend/README.md
+++ backend/README.md
@@ -9,4 +9,19 @@ Mobile client request for presigned url to upload image.
 <b>Request Structure</b>
 | Parameter | Required | Type
 | :-- | :-- | :--
+| bucket | required | string
 | key | required | string
+
+### /foodItems GET
+OCR receipt image and parse through Gemini to get the array of food items on the receipt.
+
+<b>Request Structure</b>
+| Parameter | Required | Type
+| :-- | :-- | :--
+| bucket | required | string
+| key | required | string
+
+<b>Response Structure</b>
+| Field | Type
+| :-- | :--
+| foodItems | List[string]
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,17 @@
 import boto3
 import os
+import time
+import logging
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from ocr_pipeline import OCRPipeline
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 OCR_PIPELINE = OCRPipeline(model="gemini-2.5-flash")
 S3_CLIENT = boto3.client(
@@ -31,15 +36,31 @@ class ObjectPath(BaseModel):
 
 
 @app.get("/upload")
-def get_presigned_url(req: ObjectPath):
+def get_presigned_url(bucket: str, key: str):
   """ Request a presigned url from AWS S3 """
+  return {
+    "url": S3_CLIENT.generate_presigned_url(
+      ClientMethod = 'put_object',
+      Params = {
+        "Bucket": bucket,
+        "Key": key,
+      },
+      ExpiresIn = 600
+    )
+  }
 
-  return S3_CLIENT.generate_presigned_url(
-    ClientMethod = 'put_object',
-    Params = {
-      "Bucket": req.bucket,
-      "Key": req.key,
-    },
-    ExpiresIn = 600
-  )
 
+
+@app.get("/foodItems")
+def get_food_items(req: ObjectPath):
+  """ Download the image from S3 & Run OCR Pipeline """
+
+  start = time.time()
+  #? Body is a streamingbody
+  img_bytes: bytes = S3_CLIENT.get_object(Bucket=req.bucket, Key=req.key)['Body'].read()
+  end = time.time()
+  logger.info(f"S3 Get Object: {end - start:4.f} seconds")
+
+  return {
+    "foodItems": OCR_PIPELINE.get_food_items(img_bytes)
+  }
